afeb7b0c33d1a5c4fa26134b29e874ee34d800b0
Simon Bernier St-Pierre <sbernierstpierre@gmail.com>
2014-03-03 23:14:17 -0500

    first version of tbp engine

--- engines/thepiratebay.js
+++ engines/thepiratebay.js
@@ -1,5 +1,11 @@
-// ThePirateBay engine
-//--------------------
+/**
+ * ThePirateBay engine.
+ */
+
+var http = require('../lib/http.js')
+  , common = require('../lib/common.js')
+  , util = require('util')
+  , events = require('events');
 
 // Parse a date.
 var reDate = /^Uploaded\s(\d\d)\-(\d\d)\s(\d{4})$/i
@@ -11,52 +17,80 @@ var reDate = /^Uploaded\s(\d\d)\-(\d\d)\s(\d{4})$/i
 // Parse the expected amount of results.
   , reExpectedAmount = /\(approx\s(\d+)\sfound\)$/i;
 
-// Extract the date out of a string.
+/**
+ * Extract a date from a string formatted like 12-29 2013 or 01-13 07:31.
+ * @param str Date to extract.
+ * @returns A properly formatted ISO date.
+ */
 function extractDate(str) {
-  matches = str.match(reDate);
+  var year, month, day;
+  var matches = str.match(reDate);
   if (matches !== null) {
-    return util.format('%s-%s-%s', matches[3], matches[1], matches[2]);
+    year = parseInt(matches[3]);
+    month = parseInt(matches[1]);
+    day = parseInt(matches[2]);
   } else {
     matches = str.match(reSpecialDate);
-    return util.format('%d-%s-%s', new Date().getFullYear(), matches[1],
-      matches[2]);
+    year = new Date().getFullYear();
+    month = parseInt(matches[1]);
+    day = parseInt(matches[2]);
   }
+  return common.formatDateParts(year, month, day);
 }
 
-// Extract the size out of a string.
+/**
+ * Extract the size of the torrent from a string formatted like 223 MiB.
+ * @param str Size to extract.
+ * @returns Torrent size.
+ */
 function extractSize(str) {
   return str.match(reSize)[1];
 }
 
-// Extract the expected amount of results out of a string.
+/**
+ * Extract the expected amount of results from a string formatted like approx 655 found.
+ * @param str Amount to extract.
+ * @returns Expected amount of results.
+ */
 function extractExpectedAmount(str) {
-  return parseInt(str.match(reExpectedAmount));
+  return parseInt(str.match(reExpectedAmount)[1]);
 }
 
-// Constructor of the Search object.
-// `what` is a string, what the engine is looking for.
+/**
+ * Constructor of the search object.
+ * @param what String to search.
+ */
 function Search(what) {
   this._what = what;
   this._stop = false;
   this._page = 0;
   this._resultCount = 0;
   this._expectedAmount = null;
+  this.results = [];
 }
 
-// This tells the search to start loading pages, one at a time, until there are
-// no more results to fetch or if the stop method was called.
+// extend EventEmitter
+util.inherits(Search, events.EventEmitter);
+
+/**
+ * Start searching for torrents. The search will stop when it has retrieved
+ * all of the available results, or if the stop method.
+ */
 Search.prototype.start = function () {
-  this._fetchNextPage();
+  this._nextPage();
 };
 
-// This tells the search to stop. It does not garantee that the search will stop
-// now, only that it will not load another page. The `end` event will be fired
-// when the search is truly done.
+/**
+ * Stop searching for torrents.
+ */
 Search.prototype.stop = function () {
   this._stop = true;
 };
 
-Search.prototype._fetchNextPage = function () {
+/**
+ * Get the next page of results.
+ */
+Search.prototype._nextPage = function () {
   // if the search should stop, exit the function
   if (this._stop) return;
 
@@ -64,6 +98,64 @@ Search.prototype._fetchNextPage = function () {
   // first
   var url = util.format('%s/search/%s/%d/7/0', exports.url,
     encodeURIComponent(this._what), this._page++);
+
+  // fetch the page
+  http.get(url, function (err, $) {
+
+    // extract expected amount if needed
+    if (this._expectedAmount === null) {
+      $('h2').find('span').remove();
+      this._expectedAmount = extractExpectedAmount($('h2').text());
+    }
+
+    // remove table head
+    $('#searchResult tr').eq(0).remove();
+
+    // variables needed for data extraction
+    var results = []
+      , result, parts
+      , cells, detLink, font;
+
+    // loop through rows and extract data
+    $('#searchResult tr').toArray().forEach(function (row) {
+      cells = $(row).find('td');
+      detLink = $(row).find('.detLink').eq(0);
+      font = $(cells[1]).find('font');
+
+      $(font).remove('a');
+      parts = $(font).text().split(',');
+
+      result = {
+        title: $(detLink).attr('title'),
+        url: exports.url + $(detLink).attr('href'),
+        magnet: $(cells[1]).find('a').eq(1).attr('href'),
+        date: extractDate(parts[0]),
+        size: extractSize(parts[1]),
+        seeds: parseInt($(cells[2]).text()),
+        leechs: parseInt($(cells[3]).text())
+      };
+
+      results.push(result);
+    }, this);
+
+    // append new results to all results
+    results.forEach(function (result) {
+      this.results.push(result);
+    }, this);
+
+    // emit event
+    this.emit('results', results);
+
+    // check if we're done
+    var done = this._stop || this.results.length >= this._expectedAmount;
+
+    // do not stop
+    if (done) {
+      this.emit('end');
+    } else {
+      this._nextPage();
+    }
+  }, this);
 };
 
 // Wrapper around the search object to ease its construction.
