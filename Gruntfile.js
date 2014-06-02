42cc81b75d48747435c0dffc4214770d3fd973e9
Simon Bernier St-Pierre <sbernierstpierre@gmail.com>
2014-06-01 21:53:36 -0400

    update package.json
    remove useless grunt plugins
    update dependencies
    add socket.io

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,25 +34,11 @@ module.exports = function (grunt) {
 
         node: true
       }
-    },
-    watch: {
-      files: jsFiles,
-      tasks: ['jshint'],
-      options: {
-        spawn: true
-      }
-    },
-    exec: {
-      server: {
-        cmd: 'nodemon app.js'
-      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-exec');
 
-  grunt.registerTask('default', ['exec:server']);
+  grunt.registerTask('default', ['jshint']);
 
 };
